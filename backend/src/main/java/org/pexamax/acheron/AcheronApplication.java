f8faa110b2a8c018e199f0eec1e9acea11caa4c1
fredd-deez <soksophal2004@gmail.com>
2025-05-22 10:28:00 +0700

    Added files for backend

---
+++ backend/src/main/java/org/pexamax/acheron/AcheronApplication.java
@@ -0,0 +1,13 @@
+package org.pexamax.acheron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcheronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcheronApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/pexamax/acheron/AcheronApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pexamax.acheron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcheronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
