2a6d062a3341b59577a8bd05c1b6a86fea72c09c
fredd-deez <soksophal2004@gmail.com>
2025-06-14 09:13:22 +0700

    Added setters and getters for some classes

--- backend/src/main/java/org/pexamax/acheron/Conversation.java
+++ backend/src/main/java/org/pexamax/acheron/Conversation.java
@@ -14,12 +14,41 @@ class Conversation {
     private Instant createdTimestamp;
 
     public Conversation(String user1ID, String user2ID, String user1EncSharedSecret, String user2EncSharedSecret) {
-        conversationID = lastConversationID++;
-        this.hidden = false;
-        this.user1ID = user1ID;
-        this.user2ID = user2ID;
-        this.user1EncSharedSecret = user1EncSharedSecret;
-        this.user2EncSharedSecret = user2EncSharedSecret;
-        this.createdTimestamp = Instant.now();
+        // conversationID = lastConversationID++;
+        // this.hidden = false;
+        // this.user1ID = user1ID;
+        // this.user2ID = user2ID;
+        // this.user1EncSharedSecret = user1EncSharedSecret;
+        // this.user2EncSharedSecret = user2EncSharedSecret;
+        // this.createdTimestamp = Instant.now();
+        conversationID(lastConversationID++);
+        this.hidden(false);
+        this.user1ID(user1ID);
+        this.user2ID(user2ID);
+        this.user1EncSharedSecret(user1EncSharedSecret);
+        this.user2EncSharedSecret(user2EncSharedSecret);
+        this.createdTimestamp(Instant.now());
     }
+
+    // Getters & Setters
+    public int conversationID() { return this.conversationID; }
+    public void conversationID(int conversationID) { this.conversationID = conversationID; }
+
+    public boolean hidden() { return this.hidden; }
+    public void hidden(boolean hidden) { this.hidden = hidden; }
+
+    public String user1ID() { return this.user1ID; }
+    public void user1ID(String user1ID) { this.user1ID = user1ID; }
+
+    public String user2ID() { return this.user2ID; }
+    public void user2ID(String user2ID) { this.user2ID = user2ID; }
+
+    public String user1EncSharedSecret() { return this.user1EncSharedSecret; }
+    public void user1EncSharedSecret(String user1EncSharedSecret) { this.user1EncSharedSecret = user1EncSharedSecret; }
+
+    public String user2EncSharedSecret() { return this.user2EncSharedSecret; }
+    public void user2EncSharedSecret(String user2EncSharedSecret) { this.user2EncSharedSecret = user2EncSharedSecret; }
+
+    public Instant createdTimestamp() { return this.createdTimestamp; }
+    public void createdTimestamp(Instant createdTimestamp) { this.createdTimestamp = createdTimestamp; }
 }
--- backend/src/main/java/org/pexamax/acheron/Device.java
+++ backend/src/main/java/org/pexamax/acheron/Device.java
@@ -12,10 +12,32 @@ class Device {
     private Instant deviceTokenExpiry;
 
     public Device(String sessionToken, String deviceToken, Instant sessionTokenExpiry, Instant deviceTokenExpiry) {
-        deviceID = lastDeviceID++;
-        this.sessionToken = sessionToken;
-        this.deviceToken = deviceToken;
-        this.sessionTokenExpiry = sessionTokenExpiry;
-        this.deviceTokenExpiry = deviceTokenExpiry;
+        // deviceID = lastDeviceID++;
+        // this.sessionToken = sessionToken;
+        // this.deviceToken = deviceToken;
+        // this.sessionTokenExpiry = sessionTokenExpiry;
+        // this.deviceTokenExpiry = deviceTokenExpiry;
+        this.deviceID(lastDeviceID++);
+        this.sessionToken(sessionToken);
+        this.deviceToken(deviceToken);
+        this.sessionTokenExpiry(sessionTokenExpiry);
+        this.deviceTokenExpiry(deviceTokenExpiry);
     }
+    
+    // Getters & Setters
+    public String sessionToken() { return this.sessionToken; }
+    public void sessionToken(String sessionToken) { this.sessionToken = sessionToken; }
+
+    public int deviceID() { return this.deviceID; }
+    public void deviceID(int deviceID) { this.deviceID = deviceID; }
+
+    public String deviceToken() { return this.deviceToken; }
+    public void deviceToken(String deviceToken) { this.deviceToken = deviceToken; }
+
+    public Instant sessionTokenExpiry() { return this.sessionTokenExpiry; }
+    public void sessionTokenExpiry(Instant sessionTokenExpiry) { this.sessionTokenExpiry = sessionTokenExpiry; }
+
+    public Instant deviceTokenExpiry() { return this.deviceTokenExpiry; }
+    public void deviceTokenExpiry(Instant deviceTokenExpiry) { this.deviceTokenExpiry = deviceTokenExpiry; }
+
 }
