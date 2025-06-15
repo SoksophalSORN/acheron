c62177ae020e3e90b92e00ae8767c4f101774483
fredd-deez <soksophal2004@gmail.com>
2025-06-15 22:22:19 +0700

    Added getters and setters for user.java

--- backend/src/main/java/org/pexamax/acheron/User.java
+++ backend/src/main/java/org/pexamax/acheron/User.java
@@ -12,12 +12,40 @@ class User {
     private String encPrivateKey;
 
     public User(String username, String email, String passwordHash, String publicKey, String encPrivateKey) {
-        userID = lastUserID++;
-        this.username = username;
-        this.email = email;
-        this.emailVerified = false;
-        this.passwordHash = passwordHash;
-        this.publicKey = publicKey;
-        this.encPrivateKey = encPrivateKey;
+        // userID = lastUserID++;
+        // this.username = username;
+        // this.email = email;
+        // this.emailVerified = false;
+        // this.passwordHash = passwordHash;
+        // this.publicKey = publicKey;
+        // this.encPrivateKey = encPrivateKey;
+        this.userID(lastUserID++);
+        this.username(username);
+        this.email(email);
+        this.emailVerified(false);
+        this.passwordHash(passwordHash);
+        this.publicKey(publicKey);
+        this.encPrivateKey(encPrivateKey);
     }
+    
+    public int userID() { return userID; }
+    public void userID(int userID) { this.userID = userID; }
+
+    public String username() { return username; }
+    public void username(String username) { this.username = username; }
+
+    public String email() { return email; }
+    public void email(String email) { this.email = email; }
+
+    public boolean emailVerified() { return emailVerified; }
+    public void emailVerified(boolean emailVerified) { this.emailVerified = emailVerified; }
+
+    public String passwordHash() { return passwordHash; }
+    public void passwordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public String publicKey() { return publicKey; }
+    public void publicKey(String publicKey) { this.publicKey = publicKey; }
+
+    public String encPrivateKey() { return encPrivateKey; }
+    public void encPrivateKey(String encPrivateKey) { this.encPrivateKey = encPrivateKey; }
 }
