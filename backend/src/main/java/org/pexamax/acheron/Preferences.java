ec65371b50ec0962ca92c366a856ad7f905768ff
fredd-deez <soksophal2004@gmail.com>
2025-06-15 22:11:26 +0700

    Added getters and setters for preferences.java

--- backend/src/main/java/org/pexamax/acheron/Preferences.java
+++ backend/src/main/java/org/pexamax/acheron/Preferences.java
@@ -13,9 +13,30 @@ class Preferences {
     private int defaultDestructTimer;
 
     public Preferences(int userID, String blockList, boolean selfDestruct, int defaultDestructTimer) {
-        preferenceID = lastPreferencesID++;
-        this.blockList = new ArrayList<String>(Arrays.asList(blockList.split(",")));
-        this.selfDestruct = selfDestruct;
-        this.defaultDestructTimer = defaultDestructTimer;
+        // preferenceID = lastPreferencesID++;
+        // this.blockList = new ArrayList<String>(Arrays.asList(blockList.split(",")));
+        // this.selfDestruct = selfDestruct;
+        // this.defaultDestructTimer = defaultDestructTimer;
+        this.preferenceID(lastPreferencesID++);
+        this.blockList(new ArrayList<String>(Arrays.asList(blockList.split(","))));
+        this.selfDestruct(selfDestruct);
+        this.defaultDestructTimer(defaultDestructTimer);
     }
+
+    public int preferenceID() { return this.preferenceID; }
+    public void preferenceID(int preferenceID) { this.preferenceID = preferenceID; }
+
+    public int userID() { return this.userID; }
+    public void userID(int userID) { this.userID = userID; }
+
+    public ArrayList<String> blockList() { return this.blockList; }
+    public void blockList(ArrayList<String> blockList) { this.blockList = blockList; }
+
+    public boolean selfDestruct() { return this.selfDestruct; }
+    public void selfDestruct(boolean selfDestruct) { this.selfDestruct = selfDestruct; }
+
+    public int defaultDestructTimer() { return this.defaultDestructTimer; }
+    public void defaultDestructTimer(int defaultDestructTimer) { this.defaultDestructTimer = defaultDestructTimer; }
+
+
 }
