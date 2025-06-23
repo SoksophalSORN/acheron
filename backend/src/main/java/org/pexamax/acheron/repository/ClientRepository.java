e901913e1d9df21478eb4714dc9c312c3ff2728f
fredd-deez <soksophal2004@gmail.com>
2025-06-23 19:24:16 +0700

    Wrote repos for 5 other entities/classes beside User

--- backend/src/main/java/org/pexamax/acheron/repository/ClientRepository.java
+++ backend/src/main/java/org/pexamax/acheron/repository/ClientRepository.java
@@ -1,16 +1,13 @@
 // Package and Imports
 package org.pexamax.acheron.repository;
 
-import org.pexamax.acheron.entity.User;
+import org.pexamax.acheron.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional; // Null-safe container for optional values
-import java.util.UUID; // For unique identifiers
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
     // JPA auto-generates methods for basic CRUD operations
     // Custom query methods are written below
-    Optional<User> findByEmail(String email); // Query: SELECT * FROM users WHERE email = "<email>"
-
 }
 
