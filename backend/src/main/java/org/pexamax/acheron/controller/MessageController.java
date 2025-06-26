2c85650d0fbfb39ab383cb3cb0270a4d3716e70d
fredd-deez <soksophal2004@gmail.com>
2025-06-26 14:22:32 +0700

    Added files for controllers

---
+++ backend/src/main/java/org/pexamax/acheron/controller/MessageController.java
@@ -0,0 +1,12 @@
+// Package and Imports
+package org.pexamax.acheron.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+
+}
+
