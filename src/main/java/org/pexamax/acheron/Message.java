1abe0ae3d54238f2770ea686d38adfe960418bce
fredd-deez <soksophal2004@gmail.com>
2025-07-03 18:14:33 +0700

    Removed getters and setters and any JPA related annotation

--- src/main/java/org/pexamax/acheron/Message.java
+++ src/main/java/org/pexamax/acheron/Message.java
@@ -1,26 +1,18 @@
-package org.pexamax.acheron.entity;
+package org.pexamax.acheron;
 
-import jakarta.persistence.*;
 import java.time.Instant;
 
-@Entity
 public class Message {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long messageID; // Auto-generated and auto-incremented by JPA
+	private long messageID; 
 
 	private Long conversationID;
 	private int destructTime;
 	private Instant sentTime;
-	private Instant readTime;
-	private Long senderID;
+	private Instant readTime; private Long senderID;
 	private String contentType;
 	private byte[] encContent;
 	private String digitalSignature;
-
-	// Default constructor - required by JPA
-	public Message() {}
 
 	public Message(
 		Long conversationID,
@@ -30,84 +22,14 @@ public class Message {
 		String digitalSignature,
 		int destructTime
 	) {
-		this.setConversationID(conversationID);
-		this.setSenderID(senderID);
-		this.setDestructTime(destructTime);
-		this.setSentTime(Instant.now());
-		this.setContentType(contentType);
-		this.setEncContent(encContent);
-		this.setDigitalSignature(digitalSignature);
+        this.conversationID = conversationID;
+        this.senderID = senderID;
+        this.destructTime = destructTime;
+        this.sentTime = Instant.now(); // Set sentTime to current time
+        this.readTime = null; // Initially, readTime is null
+        this.contentType = contentType;
+        this.encContent = encContent;
+        this.digitalSignature = digitalSignature;
 	}
 
-	public Long getMessageID() {
-		return this.messageID;
-	}
-
-	public void setMessageID(Long messageID) {
-		this.messageID = messageID;
-	}
-
-	public Long getConversationID() {
-		return this.conversationID;
-	}
-
-	public void setConversationID(Long conversationID) {
-		this.conversationID = conversationID;
-	}
-
-	public int getDestructTime() {
-		return this.destructTime;
-	}
-
-	public void setDestructTime(int destructTime) {
-		this.destructTime = destructTime;
-	}
-
-	public Instant getSentTime() {
-		return this.sentTime;
-	}
-
-	public void setSentTime(Instant sentTime) {
-		this.sentTime = sentTime;
-	}
-
-	public Long getSenderID() {
-		return this.senderID;
-	}
-
-	public void setSenderID(Long id) {
-		senderID = id;
-	}
-
-	public String getContentType() {
-		return this.contentType;
-	}
-
-	public void setContentType(String contentType) {
-		this.contentType = contentType;
-	}
-
-	public byte[] getEncContent() {
-		return this.encContent;
-	}
-
-	public void setEncContent(byte[] encContent) {
-		this.encContent = encContent;
-	}
-
-	public String getDigitalSignature() {
-		return this.digitalSignature;
-	}
-
-	public void setDigitalSignature(String digitalSignature) {
-		this.digitalSignature = digitalSignature;
-	}
-
-	public Instant getReadTime() {
-		return this.readTime;
-	}
-
-	public void setReadTime(Instant readTime) {
-		this.readTime = readTime;
-	}
 }
--- src/main/java/org/pexamax/acheron/Request.java
+++ src/main/java/org/pexamax/acheron/Request.java
@@ -1,22 +1,15 @@
 // Package and Imports
-package org.pexamax.acheron.entity;
+package org.pexamax.acheron;
 
-import jakarta.persistence.*;
 import java.time.Instant;
 
-@Entity
 public class Request {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long requestID; // Auto-generated and auto-incremented by JPA
+	private Long requestID; 
 
 	Long requesterID;
 	Long requesteeID;
 	Instant requestTimestamp;
-
-	// Default constructor - required by JPA
-	public Request() {}
 
 	public Request(Long requester, Long requestee) {
 		this.setRequesterID(requester);
@@ -24,35 +17,4 @@ public class Request {
 		this.setRequestTimestamp(Instant.now());
 	}
 
-	public Long getRequestID() {
-		return this.requestID;
-	}
-
-	public void setRequestID(Long requestID) {
-		this.requestID = requestID;
-	}
-
-	public Long getRequesterID() {
-		return this.requesterID;
-	}
-
-	public void setRequesterID(Long id) {
-		requesterID = id;
-	}
-
-	public Long getRequesteeID() {
-		return this.requesteeID;
-	}
-
-	public void setRequesteeID(Long id) {
-		requesteeID = id;
-	}
-
-	public Instant getRequestTimestamp() {
-		return this.requestTimestamp;
-	}
-
-	public void setRequestTimestamp(Instant requestTimestamp) {
-		this.requestTimestamp = requestTimestamp;
-	}
 }
